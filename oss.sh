d5309f60a8e6bf6315aa4a349da58ea05c5e4828
RooGhz720 <Rooghz720@gmail.com>
2022-12-03 12:50:06 +0700

    rev6

--- oss.sh
+++ oss.sh
@@ -47,7 +47,7 @@ export CHATID API_BOT TYPE_KERNEL tg_sticker
 
 # Configuration
 
-if [ "TYPE_KERNEL" == Ganteng ];
+if [ "Ganteng" == Ganteng ];
 then
 git revert 5e35eea50ec8b148a7544a9db0dafabff42132de --no-edit
 echo "Switching to OSS Version"
