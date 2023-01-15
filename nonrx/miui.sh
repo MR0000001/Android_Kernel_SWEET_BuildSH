8702e680d051e8ff34bd515eaecc40d0ea3280a9
RooGhz720 <rooghz720@gmail.com>
2023-01-15 11:44:16 +0800

    miui: change to cosmic clang:

--- nonrx/miui.sh
+++ nonrx/miui.sh
@@ -97,7 +97,7 @@ tg_error() {
 
 # clang stuff
 		echo -e "$green << cloning clang >> \n $white"
-		git clone --depth=1 -b clang-15 https://gitlab.com/LeCmnGend/proton-clang.git "$HOME"/clang
+		git clone --depth=1 -b release/15.x https://gitlab.com/GhostMaster69-dev/cosmic-clang.git "$HOME"/clang
 
 	export PATH="$HOME/clang/bin:$PATH"
 	export KBUILD_COMPILER_STRING=$("$HOME"/clang/bin/clang --version | head -n 1 | sed -e 's/  */ /g' -e 's/[[:space:]]*$//' -e 's/^.*clang/clang/')
