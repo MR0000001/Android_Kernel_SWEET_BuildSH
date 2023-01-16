ce357d33d15a042a62fcaaf78cb9ef00b34f32da
RooGhz720 <rooghz720@gmail.com>
2023-01-16 16:54:44 +0800

    oss: switch to cosmic clang

--- nonrx/oss.sh
+++ nonrx/oss.sh
@@ -106,7 +106,7 @@ tg_error() {
 
 # clang stuff
 		echo -e "$green << cloning clang >> \n $white"
-		git clone --depth=1 -b Neutron-14 https://github.com/RooGhz720/neutron-clang.git "$HOME"/clang
+		git clone --depth=1 -b release/15.x https://gitlab.com/GhostMaster69-dev/cosmic-clang.git "$HOME"/clang
 
 	export PATH="$HOME/clang/bin:$PATH"
 	export KBUILD_COMPILER_STRING=$("$HOME"/clang/bin/clang --version | head -n 1 | sed -e 's/  */ /g' -e 's/[[:space:]]*$//' -e 's/^.*clang/clang/')
